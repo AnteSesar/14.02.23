4b587e5eab6cd597e2482a122cc7f5329a415d71
Ante Sesar <ante.sesar11@gmail.com>
2023-02-14 17:55:50 +0100

    1

---
+++ 1/java.js
@@ -0,0 +1,14 @@
+var firstName, lastName, price, discount, fullPrice;
+firstName = "John";
+lastName = "Doe";
+price = 19.90;
+discount = 0.10;
+
+var x = 0;
+if (x == 1){ /* == znači uspoređujemo (postoji i ===) */
+    x = 2;
+}
+if (a) {
+    var z = 44
+        return 0;
+}
