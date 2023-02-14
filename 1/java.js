08a8c1da16abaf10179e71a5c2cf5d58c3ad9a76
Ante Sesar <ante.sesar11@gmail.com>
2023-02-14 19:24:26 +0100

    end

--- 1/java.js
+++ 1/java.js
@@ -5,10 +5,16 @@ price = 19.90;
 discount = 0.10;
 
 var x = 0;
-if (x == 1){ /* == znači uspoređujemo (postoji i ===) */
+if (x == 1){
     x = 2;
 }
+
+var a = true
 if (a) {
-    var z = 44
-        return 0;
+    var z = 44;
+    return 0;
+}
+
+if (discount != 0){
+fullPrice = price * 100 / discount;
 }
